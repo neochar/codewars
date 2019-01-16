52ee91fdd4d6dffbd488107a39ae6e7c89065c67
neochar <mail@neochar.com>
2019-01-16 22:09:05 +0300

    Packet delivery solution updated.

--- packet-delivery/main.py
+++ packet-delivery/main.py
@@ -1,55 +1,39 @@
+class DimensionsOutOfBound(Exception):
+
+    def __init__(self, dimension, value, limit):
+        self.str = "Package {}=={} out of bound, should be: " \
+                "0 < {} <= {}".format(
+                        dimension,
+                        value,
+                        dimension,
+                        limit
+                        )
+
+    def __str__(self):
+        return self.str
+
 class Package(object):
 
+    limits = {
+        "length": 350,
+        "width":  300,
+        "height": 150,
+        "weight": 40
+    }
+
     def __init__(self, length, width, height, weight=None):
-        self._length = length
-        self._width  = width
-        self._height = height
-        self._weight = weight
+        self.length = length
+        self.width  = width
+        self.height = height
+        if weight:
+            self.weight = weight
+
+    def __setattr__(self, attribute, value):
+        if not (0 < value <= Package.limits[attribute]):
+            raise DimensionsOutOfBound(attribute, value, self.limits[attribute])
+        self.__dict__[attribute] = value
 
     @property
     def volume(self):
-        return self._length * self._width * self._height
+        return self.width * self.height * self.length
 
-    @property
-    def length(self):
-        return self._length
-
-    @length.setter
-    def length(self, value):
-        if value <= 0 or value > 350:
-            raise DimensionsOutOfBoundError('Package length==% out of bound, should be: 0 < length <= 350'.format(value))
-        self._length = value
-
-    @property
-    def width(self):
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        if 0 >= value > 300:
-            raise DimensionsOutOfBoundError('Package width==% out of bound, should be: 0 < width <= 300'.format(value))
-        self._width = value
-
-    @property
-    def height(self):
-        return self._height
-
-    @height.setter
-    def height(self, value):
-        if 0 >= value > 150:
-            raise DimensionsOutOfBoundError('Package height==% out of bound, should be: 0 < height <= 150'.format(value))
-        self._height = value
-
-    @property
-    def weight(self):
-        return self._weight
-
-    @weight.setter
-    def weight(self, value):
-        if 0 >= value > 40:
-            raise DimensionsOutOfBoundError('Package weight==% out of bound, should be: 0 < weight <= 40'.format(value))
-        self._weight = value
-
-p = Package(0.2, 0.1, 0.2)
-p.length = -20
-print(p.length)
